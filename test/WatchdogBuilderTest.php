47e28ecb79c442213da04369f86bd82a4b929d30
Krzysztof Kubanek <krzysztof.kubanek@gmail.com>
2022-07-26 15:30:36 +0200

    tests: adds WatchdogBuilder unit test

---
+++ test/WatchdogBuilderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AgDevelop\ForkingSupervisor\Test;
+
+use AgDevelop\ForkingSupervisor\Watchdog\Watchdog;
+use AgDevelop\ForkingSupervisor\Watchdog\WatchdogBuilder;
+use PHPUnit\Framework\TestCase;
+
+class SampleWatchdog extends Watchdog
+{
+
+}
+
+class WatchdogBuilderTest extends TestCase
+{
+
+
+    public function testBuild()
+    {
+        $builder = new WatchdogBuilder(10, 10, SampleWatchdog::class);
+
+        $actual = $builder->build();
+
+        $this->assertInstanceOf(SampleWatchdog::class, $actual);
+    }
+}
