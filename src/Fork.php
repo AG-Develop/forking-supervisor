0ba08c8e7fe77eb4ec33aa92930503175a5cc389
Krzysztof Kubanek <krzysztof.kubanek@gmail.com>
2022-08-18 11:54:09 +0200

    feat: sets Fork properties to readonly

--- src/Fork.php
+++ src/Fork.php
@@ -8,9 +8,9 @@ use AgDevelop\ForkingSupervisor\Watchdog\WatchdogInterface;
 class Fork
 {
     public function __construct(
-        private int $processId,
-        private JobInterface $job,
-        private WatchdogInterface $watchdog,
+        private readonly int $processId,
+        private readonly JobInterface $job,
+        private readonly WatchdogInterface $watchdog,
     ) {
     }
 
